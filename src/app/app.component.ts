dbd4bd3e3ede1923a949763d6ae1d2586f82fe49
DESKTOP-IKGKPUC\Alvca <alv.carballo@outlook.es>
2022-03-01 16:46:08 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DWC_Tema4_EjemplosAngular';
+  title = 'Tour of Heroes';
+  subTitle ='Bienvenidos Heroes';
 }
